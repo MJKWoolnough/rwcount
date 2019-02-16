885d59e9d9c31b44d982ac382e8aa7c127d96042
Michael Woolnough <michael.woolnough@gmail.com>
2019-02-16 10:36:35 +0000

    rewrote tests

--- rwcount_test.go
+++ rwcount_test.go
@@ -1,50 +1,37 @@
 package rwcount
 
 import (
-	"bytes"
 	"testing"
 )
 
-type Reader interface {
-	Read([]byte) (int, error)
+type null struct{}
+
+func (null) Read(p []byte) (int, error) {
+	return len(p), nil
 }
 
-type Writer interface {
-	Write([]byte) (int, error)
-}
-
-func read(from Reader, data []byte) (total int64, read int) {
-	cR := &CountReader{Reader: from}
-	read, _ = cR.Read(data)
-	return cR.Count, read
-}
-
-func write(to Writer, data []byte) (total int64, written int) {
-	cW := &CountWriter{Writer: to}
-	written, _ = cW.Write(data)
-	return cW.Count, written
+func (null) Write(p []byte) (int, error) {
+	return len(p), nil
 }
 
 func TestCounts(t *testing.T) {
-	var (
-		buf   []byte
-		total int64
-		n     int
-	)
-	b := new(bytes.Buffer)
+	data := make([]byte, 0, 1500)
+	r := Reader{Reader: null{}}
+	w := Writer{Writer: null{}}
+	var total int64
 	for i := 0; i < 1500; i++ {
-		buf = make([]byte, i)
-		total, n = write(b, buf)
+		total += int64(i)
+		n, _ := w.Write(data[:i])
 		if n != i {
 			t.Errorf("failed to write %d bytes to buffer, wrote %d", i, n)
-		} else if total != int64(i) {
-			t.Errorf("written bytes returned (%d) didn't match expected (%d)", total, i)
+		} else if total != w.Count {
+			t.Errorf("written bytes returned (%d) didn't match expected (%d)", w.Count, total)
 		}
-		total, n = read(b, buf)
+		n, _ = r.Read(data[:i])
 		if n != i {
 			t.Errorf("failed to read %d bytes from buffer, read %d", i, n)
-		} else if total != int64(i) {
-			t.Errorf("read bytes returned (%d) didn't match expected (%d)", total, i)
+		} else if total != r.Count {
+			t.Errorf("read bytes returned (%d) didn't match expected (%d)", r.Count, total)
 		}
 	}
 }
