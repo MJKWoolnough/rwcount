41a024a8e5bf2a0761013ca4b004bdef450953c1
Michael Woolnough <michael.woolnough@gmail.com>
2024-06-06 21:31:07 +0100

    cleanup

--- rwcount.go
+++ rwcount.go
@@ -4,49 +4,55 @@ package rwcount // import "vimagination.zapto.org/rwcount"
 
 import "io"
 
-// Reader is used to wrap a io.Reader for counting
+// Reader is used to wrap a io.Reader for counting.
 type Reader struct {
 	io.Reader
 	Count int64
 	Err   error
 }
 
-// Read implements the io.Reader interface
+// Read implements the io.Reader interface.
 func (c *Reader) Read(d []byte) (int, error) {
 	if c.Err != nil {
 		return 0, c.Err
 	}
+
 	total, err := c.Reader.Read(d)
 	c.Count += int64(total)
 	c.Err = err
+
 	return total, err
 }
 
-// Writer is used to wrap a io.Writer for counting
+// Writer is used to wrap a io.Writer for counting.
 type Writer struct {
 	io.Writer
 	Count int64
 	Err   error
 }
 
-// Write implements the io.Writer interface
+// Write implements the io.Writer interface.
 func (c *Writer) Write(d []byte) (int, error) {
 	if c.Err != nil {
 		return 0, c.Err
 	}
+
 	total, err := c.Writer.Write(d)
 	c.Count += int64(total)
 	c.Err = err
+
 	return total, err
 }
 
-// WriteString implements the io.StringWriter interface
+// WriteString implements the io.StringWriter interface.
 func (c *Writer) WriteString(s string) (int, error) {
 	if c.Err != nil {
 		return 0, c.Err
 	}
+
 	total, err := io.WriteString(c.Writer, s)
 	c.Count += int64(total)
 	c.Err = err
+
 	return total, err
 }
--- rwcount_test.go
+++ rwcount_test.go
@@ -18,15 +18,19 @@ func TestCounts(t *testing.T) {
 	data := make([]byte, 0, 1500)
 	r := Reader{Reader: null{}}
 	w := Writer{Writer: null{}}
+
 	var total int64
+
 	for i := 0; i < 1500; i++ {
 		total += int64(i)
 		n, _ := w.Write(data[:i])
+
 		if n != i {
 			t.Errorf("failed to write %d bytes to buffer, wrote %d", i, n)
 		} else if total != w.Count {
 			t.Errorf("written bytes returned (%d) didn't match expected (%d)", w.Count, total)
 		}
+
 		n, _ = r.Read(data[:i])
 		if n != i {
 			t.Errorf("failed to read %d bytes from buffer, read %d", i, n)
